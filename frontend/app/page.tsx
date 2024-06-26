7454b563b0695549a710792e6c7de6fcf7b8751a
hrqmonteiro <hrqmonteiro@protonmail.com>
2024-06-26 20:38:40 -0300

    feat: add modal

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,10 +1,120 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+'use client'
+
+import { useRef, useState } from 'react'
+import { useRouter } from 'next/navigation'
+
+import Modal from '@/components/modal'
+
+interface MoodData {
+  mood: string
+  date: string
+}
 
 export default function Home() {
+  const [moodList, setMoodList] = useState<MoodData[]>([
+    {
+      mood: 'pleasant',
+      date: '2023-11-01T21:10:13.806Z'
+    },
+    {
+      mood: 'excited',
+      date: '2023-11-01T21:10:15.602Z'
+    },
+    {
+      mood: 'sad',
+      date: '2023-11-01T21:10:17.097Z'
+    },
+    {
+      mood: 'excited',
+      date: '2023-11-01T21:10:18.673Z'
+    },
+    {
+      mood: 'sad',
+      date: '2023-11-01T21:10:20.001Z'
+    },
+    {
+      mood: 'pleasant',
+      date: '2023-11-01T21:10:21.721Z'
+    },
+    {
+      mood: 'excited',
+      date: '2023-11-01T21:10:23.848Z'
+    },
+    {
+      mood: 'sad',
+      date: '2023-11-01T21:10:25.134Z'
+    },
+    {
+      mood: 'pleasant',
+      date: '2023-11-01T21:10:26.492Z'
+    },
+    {
+      mood: 'excited',
+      date: '2023-11-01T21:10:27.618Z'
+    },
+    {
+      mood: 'sad',
+      date: '2023-11-01T21:10:28.652Z'
+    },
+    {
+      mood: 'pleasant',
+      date: '2023-11-01T21:10:29.832Z'
+    },
+    {
+      mood: 'excited',
+      date: '2023-11-01T21:10:31.175Z'
+    },
+    {
+      mood: 'sad',
+      date: '2023-11-01T21:10:32.475Z'
+    },
+    {
+      mood: 'pleasant',
+      date: '2023-11-01T21:10:33.673Z'
+    },
+    {
+      mood: 'pleasant',
+      date: '2023-11-01T21:10:37.347Z'
+    },
+    {
+      mood: 'excited',
+      date: '2023-11-01T21:10:38.868Z'
+    },
+    {
+      mood: 'sad',
+      date: '2023-11-01T21:10:40.804Z'
+    },
+    {
+      mood: 'excited',
+      date: '2023-11-01T21:10:42.535Z'
+    },
+    {
+      mood: 'pleasant',
+      date: '2023-11-01T21:10:44.098Z'
+    }
+  ])
+
+  const [showModal, setShowModal] = useState(false)
+  const router = useRouter()
+  const scrollToRef = useRef<HTMLDivElement>(null)
+
+  const updateMood = (newMood: string) => {
+    const date = new Date().toISOString()
+    setMoodList((currentMood) => [...currentMood, { mood: newMood, date }])
+    router.push(`/?mood=${newMood}`)
+    setShowModal(false)
+    if (scrollToRef.current) {
+      scrollToRef.current.scrollIntoView({ behavior: 'smooth' })
+    }
+  }
+
   return (
-    <main >
-      <div className="bg-red-500">a</div>
+    <main>
+      <Modal
+        showModal={showModal}
+        updateMood={updateMood}
+        closeModal={() => setShowModal(false)}
+      />
     </main>
-  );
+  )
 }
--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -7,22 +7,28 @@ export default {
   ],
   theme: {
     extend: {
+      borderRadius: {
+        large: '32px'
+      },
       colors: {
         white: '#ffffff',
         lavender: {
           100: '#f1f5fe',
           200: '#c2c0f9',
           '200-opacity': '#c2c0fa7f',
-          300: '#8582dd',
+          300: '#8582dd'
         },
         midnight: {
           100: '#f5f6f4',
           200: '#c6c5c5',
           300: '#6c757d',
-          400: '#2e2f39',
-        },
+          400: '#2e2f39'
+        }
       },
-    },
+      spacing: {
+        '90': '90vw'
+      }
+    }
   },
   plugins: []
 } satisfies Config
